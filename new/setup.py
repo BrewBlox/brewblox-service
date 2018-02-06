85166795e7e3fa85fe3fc8c5e06e2f4e39442b98
Bob Steers <steers.bob@gmail.com>
2018-02-06 15:40:23 +0100

    scaffolding brewblox-service

---
+++ new/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='brewblox-service',
+    version='0.1',
+    long_description=open('README.md').read(),
+    url='https://github.com/BrewBlox/brewblox-service',
+    author='BrewPi',
+    author_email='elco@brewpi.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: System :: Hardware',
+    ],
+    keywords='brewing brewpi brewblox embedded',
+    packages=find_packages(exclude=['test']),
+    install_requires=[],
+    extras_require={'dev': ['tox']}
+)
