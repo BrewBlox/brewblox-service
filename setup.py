26ef4dea31bb395122bf19c7546a151519218772
Bob Steers <kargathia@hotmail.com>
2018-04-10 12:49:17 +0200

    Bump version: 0.6.2 → 0.7.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='brewblox-service',
-    version='0.6.2',
+    version='0.7.0',
     long_description=open('README.md').read(),
     url='https://github.com/BrewBlox/brewblox-service',
     author='BrewPi',
