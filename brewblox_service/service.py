d7d88c5b640466a289932b0af1193acb28abaec8
Bob Steers <kargathia@hotmail.com>
2018-05-14 14:16:07 +0200

    add more docstrings

--- brewblox_service/service.py
+++ brewblox_service/service.py
@@ -1,14 +1,34 @@
 """
-Generic startup for a brewblox application.
+Generic startup functions for a brewblox application.
 
 Responsible for parsing user configuration, and creating top-level objects.
+This module provides the framework to which service implementations can attach their features.
+
+Example:
+    # Uses the default argument parser
+    # To add new commandline arguments, use create_parser()
+    app = service.create_app(default_name='my_service')
+
+    # (Placeholder names)
+    # All features (endpoints and async handlers) must be created and added to the app here
+    # The Aiohttp Application will freeze functionality once it has been started
+    feature_one.setup(app)
+    feature_two.setup(app)
+
+    # Modify added resources to conform to standards
+    service.furnish(app)
+
+    # Run the application
+    # This function blocks until the application is shut down
+    service.run(app)
 """
 
 import argparse
 import logging
+# The argumentparser can't fall back to the default sys.argv if sys is not imported
 import sys  # noqa
 from logging.handlers import TimedRotatingFileHandler
-from typing import Type, List
+from typing import List
 
 import aiohttp_cors
 import aiohttp_swagger
@@ -19,7 +39,7 @@ routes = web.RouteTableDef()
 
 
 @routes.get('/_service/status')
-async def healthcheck(request: Type[web.Request]) -> Type[web.Response]:
+async def healthcheck(request: web.Request) -> web.Response:
     """
     ---
     tags:
@@ -36,7 +56,7 @@ async def healthcheck(request: Type[web.Request]) -> Type[web.Response]:
     return web.json_response({'status': 'ok'})
 
 
-def _init_logging(args: Type[argparse.Namespace]):
+def _init_logging(args: argparse.Namespace):
     level = logging.DEBUG if args.debug else logging.INFO
     format = '%(asctime)s %(levelname)-8s %(name)-30s  %(message)s'
     datefmt = '%Y/%m/%d %H:%M:%S'
@@ -61,7 +81,20 @@ def _init_logging(args: Type[argparse.Namespace]):
         logging.getLogger('aiohttp.access').setLevel(logging.WARN)
 
 
-def create_parser(default_name: str) -> Type[argparse.ArgumentParser]:
+def create_parser(default_name: str) -> argparse.ArgumentParser:
+    """
+    Creates the default brewblox_service ArgumentParser.
+    Service-agnostic arguments are added.
+
+    The parser allows calling code to add additional arguments before using it in create_app()
+
+    Args:
+        default_name (str): default value for the --name commandline argument.
+
+    Returns:
+        argparse.ArgumentParser: a Python ArgumentParser with defaults set.
+
+    """
     argparser = argparse.ArgumentParser()
     argparser.add_argument('-H', '--host',
                            help='Host to which the app binds. [%(default)s]',
@@ -83,9 +116,31 @@ def create_parser(default_name: str) -> Type[argparse.ArgumentParser]:
 
 def create_app(
         default_name: str=None,
-        parser: Type[argparse.ArgumentParser]=None,
+        parser: argparse.ArgumentParser=None,
         raw_args: List[str]=None
-) -> Type[web.Application]:
+) -> web.Application:
+    """
+    Creates and configures an Aiohttp application.
+
+    Args:
+        default_name (str, optional):
+            Default value for the --name commandline argument.
+            This value is required if `parser` is not provided.
+            This value will be ignored if `parser` is provided.
+
+        parser (argparse.ArgumentParser, optional):
+            Application-specific parser.
+            If not provided, the return value of `create_parser()` will be used.
+
+        raw_args (list of str, optional):
+            Explicit commandline arguments.
+            Defaults to sys.argv[1:]
+
+    Returns:
+        web.Application: A configured Aiohttp Application object.
+            This Application must be furnished, and is not yet running.
+
+    """
 
     if parser is None:
         assert default_name, 'Default service name is required'
@@ -100,7 +155,16 @@ def create_app(
     return app
 
 
-def furnish(app: Type[web.Application]):
+def furnish(app: web.Application):
+    """
+    Configures Application routes, readying it for running.
+
+    This function modifies routes and resources that were added by calling code,
+    and must be called immediately prior to `run(app)`.
+
+    Args:
+        app (web.Application): The Aiohttp Application as created by `create_app()`
+    """
     prefix = '/' + app['config']['name'].lstrip('/')
     app.router.add_routes(routes)
 
@@ -145,7 +209,14 @@ def furnish(app: Type[web.Application]):
         LOGGER.info(f'Registered [{route.method}] {route.resource}')
 
 
-def run(app: Type[web.Application]):
+def run(app: web.Application):
+    """
+    Runs the application in an async context.
+    This function will block indefinitely until the application is shut down.
+
+    Args:
+        app (web.Application): The Aiohttp Application as created by `create_app()`
+    """
     host = app['config']['host']
     port = app['config']['port']
 
