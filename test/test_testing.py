201a71f7918d3f8cb6bbf90625fd2a8da99ce2ce
Bob Steers <steers.bob@gmail.com>
2020-06-30 23:11:24 +0200

    Add --bind-http-server arg

--- test/test_testing.py
+++ test/test_testing.py
@@ -2,6 +2,8 @@
 Tests brewblox_service.testing
 """
 
+from unittest.mock import Mock
+
 import pytest
 
 from brewblox_service import service, testing
@@ -17,3 +19,15 @@ async def test_response(app, client):
     await testing.response(client.get('/test_app/_service/status'))
     with pytest.raises(AssertionError):
         await testing.response(client.get('/test_app/_service/status'), 400)
+
+
+def test_matching():
+    obj = testing.matching(r'.art')
+    assert obj == 'cart'
+    assert obj == 'part'
+    assert obj != 'car'
+    assert obj != ''
+
+    mock = Mock()
+    mock('fart')
+    mock.assert_called_with(obj)
