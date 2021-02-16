a5d97633734aa3448cc31df5e59dbcb3989cbdc4
Bob Steers <steers.bob@gmail.com>
2021-02-16 14:53:42 +0100

    dynamic delay in test_listen

--- test/test_mqtt.py
+++ test/test_mqtt.py
@@ -29,7 +29,7 @@ def broker(find_free_port):
 
 @pytest.fixture
 def app(app, mocker, broker):
-    app['config']['mqtt_host'] = 'localhost'
+    app['config']['mqtt_host'] = '0.0.0.0'
     app['config']['mqtt_protocol'] = 'mqtt'
     app['config']['mqtt_port'] = broker['mqtt']
 
@@ -43,7 +43,7 @@ def app(app, mocker, broker):
 @pytest.fixture
 async def connected(app, client, broker):
     try:
-        await asyncio.wait_for(mqtt.handler(app)._connect_ev.wait(), timeout=30)
+        await asyncio.wait_for(mqtt.handler(app)._connect_ev.wait(), timeout=5)
     except asyncio.TimeoutError:
         print(check_output('docker ps', shell=True).decode())
         raise
@@ -169,7 +169,15 @@ async def test_listen(app, client, connected, manual_handler):
 
     manual_handler.client.publish('brewcast/invalid', '{')
 
-    await asyncio.sleep(3)
+    delay_count = 0
+    while (cb1.await_count < 4
+            or cb2.await_count < 2
+           or cb3.await_count < 1
+           or cbh1.await_count < 3):
+        delay_count += 1
+        if delay_count >= 10:
+            break
+        await asyncio.sleep(1)
 
     cb1.assert_has_awaits([
         call('brewcast/state/test', {}),
